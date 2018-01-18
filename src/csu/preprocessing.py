c6b7ad6e5b8c654259122718eb4d2d8d92069800
windweller <leo.niecn@gmail.com>
2018-01-17 18:32:44 -0800

    add csu

---
+++ src/csu/preprocessing.py
@@ -0,0 +1,136 @@
+"""
+Takes in raw data and preprocess this into good format
+for TorchText to train on
+"""
+
+import re
+import numpy as np
+
+np.random.seed(1234)
+
+# ======== Split =========
+train_size = 0.9
+
+assert (train_size < 1 and train_size > 0)
+split_proportions = {
+    "train": train_size,
+    "valid": (1 - train_size) / 2,
+    "test": (1 - train_size) / 2
+}
+assert (sum([split_proportions[split] for split in split_proportions]) == 1)
+
+print("the data split is: {}".format(split_proportions))
+
+
+# maybe not predicting 17 (it's a catch-all disease)
+
+def write_to_tsv(data, file_name):
+    with open(file_name, 'wb') as f:
+        for line in data:
+            f.write(line[0] + '\t' + line[1] + '\n')
+
+
+def count_freq(list_labels):
+    dic = {}
+    for l in list_labels:
+        if l not in dic:
+            dic[l] = 1
+        else:
+            dic[l] += 1
+    return dic
+
+
+def get_most_freq_label(dic):
+    most_f_l = None
+    most_f_f = 0.
+    for l, f in dic.iteritems():
+        if f > most_f_f:
+            most_f_l = l
+    return most_f_l
+
+
+def get_majority_label(labels):
+    # as the first pass, we only predict the majority label
+    labels = labels.strip()
+    labels = labels.replace('17', '')  # we also ignore most-frequent non-meaningful label
+    list_labels = filter(lambda l: len(l) > 0, labels.split('-'))
+    if len(list_labels) == 0:
+        list_labels = ['17']  # meaning it only has 17
+    dic = count_freq(list_labels)
+    most_freq_label = get_most_freq_label(dic)
+    return most_freq_label
+
+
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
+def preprocess_text(text):
+    no_html = cleanhtml(text)
+    one_white_space = ' '.join(no_html.split())
+    no_html_entities = re.sub('&[a-z]+;', '', one_white_space)
+
+    return no_html_entities
+
+
+if __name__ == '__main__':
+    header = True
+
+    examples = []
+    labels_dist = []
+    with open("../../data/csu/final_csu_file", 'r') as f:
+        for line in f:
+            if header:
+                header = False
+                continue
+            columns = line.split('\t')
+            labels = columns[-1]
+            maj_label = get_majority_label(labels)
+            text = columns[3]
+
+            labels_dist.append(maj_label)
+
+            examples.append([text, maj_label])
+
+    # import matplotlib.pyplot as plt
+    #
+    # n, bins, patches = plt.hist(map(lambda x: int(x), labels_dist), 50, normed=1, facecolor='green', alpha=0.75)
+    # plt.show()
+
+    labels_dist = count_freq(labels_dist)
+
+    print("number of labels is {}".format(labels_dist))
+
+    labels_prob = map(lambda t: (t[0], float(t[1]) / sum(labels_dist.values())), labels_dist.items())
+
+    print("distribution is {}".format(labels_prob))
+
+    # process them into tsv format, but also collect frequency distribution
+    serial_numbers = range(len(examples))
+    np.random.shuffle(serial_numbers)
+
+    train_numbers = serial_numbers[:int(np.rint(len(examples) * split_proportions['train']))]
+    valid_numbers = serial_numbers[
+                    int(np.rint(len(examples) * split_proportions['train'])): \
+                        int(np.rint(len(examples) * (split_proportions['train'] + split_proportions['valid'])))]
+    test_numbers = serial_numbers[
+                   int(np.rint(len(examples) * (split_proportions['train'] + split_proportions['valid']))):]
+
+    print(
+        "train/valid/test number of examples: {}/{}/{}".format(len(train_numbers), len(valid_numbers),
+                                                               len(test_numbers)))
+
+    train, valid, test = [], [], []
+
+    for tn in train_numbers:
+        train.append(examples[tn])
+    for tn in valid_numbers:
+        valid.append(examples[tn])
+    for tn in test_numbers:
+        test.append(examples[tn])
+
+    write_to_tsv(train, "../../data/csu/maj_label_train.tsv")
+    write_to_tsv(valid, "../../data/csu/maj_label_valid.tsv")
+    write_to_tsv(test, "../../data/csu/maj_label_test.tsv")
---
+++ src/csu/text_classifier.py
@@ -0,0 +1,487 @@
+"""
+A small multiclass text classifier
+from PyTorch
+"""
+
+import sys
+import os
+import argparse
+import time
+import random
+import math
+import json
+from os.path import join as pjoin
+
+from torch.nn.utils.rnn import pad_packed_sequence as unpack
+
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchtext import data
+
+import logging
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+argparser = argparse.ArgumentParser(sys.argv[0], conflict_handler='resolve')
+argparser.add_argument("--dataset", type=str, default='major', help="major|seq, majority label or sequential label")
+argparser.add_argument("--batch_size", "--batch", type=int, default=32)
+argparser.add_argument("--unroll_size", type=int, default=35)
+argparser.add_argument("--max_epoch", type=int, default=5)
+argparser.add_argument("--d", type=int, default=910)
+argparser.add_argument("--dropout", type=float, default=0.3,
+                       help="dropout of word embeddings and softmax output")
+argparser.add_argument("--depth", type=int, default=1)
+argparser.add_argument("--lr", type=float, default=1.0)
+argparser.add_argument("--clip_grad", type=float, default=5)
+argparser.add_argument("--run_dir", type=str, default='./exp')
+argparser.add_argument("--seed", type=int, default=123)
+argparser.add_argument("--gpu", type=int, default=-1)
+argparser.add_argument("--attn", action="store_true", help="by using attention to generate some interpretation")
+argparser.add_argument("--sparsity_reg", action="store_true",
+                       help="apply L1 and diff regularization to attention logits")
+argparser.add_argument("--emb_update", action="store_true", help="update embedding")
+argparser.add_argument("--sparsity", type=float, default=4e-4, help="{2e-4, 3e-4, 4e-4}")
+argparser.add_argument("--coherent", type=float, default=2.0, help="paper did 2 * lambda1")
+argparser.add_argument("--rand_unk", action="store_true", help="randomly initialize unk")
+
+args = argparser.parse_args()
+
+VERY_NEGATIVE_NUMBER = -1e30
+
+"""
+Seeding
+"""
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+
+use_cuda = torch.cuda.is_available()
+if use_cuda:
+    torch.cuda.manual_seed_all(args.seed)
+
+"""
+Logging
+"""
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if not os.path.exists(args.run_dir):
+    os.makedirs(args.run_dir)
+file_handler = logging.FileHandler("{0}/log.txt".format(args.run_dir))
+logging.getLogger().addHandler(file_handler)
+
+logger.info(args)
+
+
+def move_to_cuda(th_var):
+    if torch.cuda.is_available():
+        return th_var.cuda(args.gpu)
+    else:
+        return th_var
+
+
+class Model(nn.Module):
+    def __init__(self, vocab, emb_dim=100, hidden_size=256, depth=1, nclasses=5,
+                 scaled_dot_attn=False, temp_max_pool=False, attn_head=1):
+        super(Model, self).__init__()
+        self.scaled_dot_attn = scaled_dot_attn
+        self.temp_max_pool = temp_max_pool
+        self.hidden_size = hidden_size
+        self.drop = nn.Dropout(0.2)
+        self.encoder = nn.LSTM(
+            emb_dim,
+            hidden_size,
+            depth,
+            dropout=0.2,
+            bidirectional=False)  # ha...not even bidirectional
+        d_out = hidden_size
+        self.out = nn.Linear(d_out * attn_head, nclasses, bias=False)
+        self.embed = nn.Embedding(len(vocab), emb_dim)
+        self.embed.weight.data.copy_(vocab.vectors)
+        self.embed.weight.requires_grad = True if args.emb_update else False
+
+        if self.scaled_dot_attn:
+            logging.info("adding scaled dot attention matrix")
+            self.key_w = nn.Parameter(torch.randn(hidden_size, hidden_size))
+
+    def create_mask(self, lengths):
+        # lengths would be a python list here, not a Tensor
+        # [max_len, batch_size]
+        masks = np.ones([max(lengths), len(lengths)], dtype='float32')
+        for i, l in enumerate(lengths):
+            masks[l:, i] = 0.
+        return torch.from_numpy(masks)
+
+    def exp_mask(self, val, mask):
+        """Give very negative number to unmasked elements in val.
+        For example, [-3, -2, 10], [True, True, False] -> [-3, -2, -1e9].
+        Typically, this effectively masks in exponential space (e.g. softmax)
+        Args:
+            val: values to be masked
+            mask: masking boolean tensor, same shape as tensor
+            name: name for output tensor
+        Returns:
+            Same shape as val, where some elements are very small (exponentially zero)
+        """
+        exp_mask = Variable((1 - mask) * VERY_NEGATIVE_NUMBER, requires_grad=False)
+        return val + move_to_cuda(exp_mask)
+
+    def compute_sparsity_penalty(self, attn_logits):
+        # attn_logits: [seq_len, batch_size]
+        #
+        coherent_factor = args.sparsity * args.coherent
+
+        # this is the l1 loss
+        z_sum = torch.sum(torch.abs(attn_logits), dim=0)  # temporal dimension
+        z_diff = torch.abs(attn_logits[1:] - attn_logits[:-1]).sum(dim=0)
+
+        sparsity_coherence_cost = torch.mean(z_sum) * args.sparsity + torch.mean(z_diff) * coherent_factor
+
+        return sparsity_coherence_cost
+
+    def forward(self, input, lengths=None):
+        embed_input = self.embed(input)
+
+        packed_emb = embed_input
+        if lengths is not None:
+            lengths = lengths.view(-1).tolist()
+            packed_emb = nn.utils.rnn.pack_padded_sequence(embed_input, lengths)
+
+        output, hidden = self.encoder(packed_emb)  # embed_input
+
+        if lengths is not None:
+            output = unpack(output)[0]
+
+        if self.temp_max_pool:
+            hidden = torch.max(output, 0)[0].squeeze(0)
+        elif self.scaled_dot_attn:
+            # add scaled dot product attention
+
+            # enc_output = output[-1]
+            enc_output = torch.squeeze(hidden[0])
+            # (batch_size, hidden_state)
+            keys = torch.mm(enc_output, self.key_w) / np.sqrt(self.hidden_size)
+
+            # (time, batch_size, hidden_state) * (1, batch_size, hidden_state)
+            keys = torch.sum(keys.view(1, -1, self.hidden_size) * output, 2)
+
+            batch_mask = self.create_mask(lengths)
+
+            masked_keys = keys * Variable(move_to_cuda(batch_mask))  # taking masked parts to be 0
+            sparsity_coherence_cost = self.compute_sparsity_penalty(masked_keys)
+
+            exp_maksed_keys = self.exp_mask(keys, batch_mask)
+
+            # (time, batch_size) -> (batch_size, time)
+            keys = torch.nn.Softmax()(torch.transpose(exp_maksed_keys, 0, 1))  # boradcast?
+            keys_view = torch.transpose(keys, 0, 1).contiguous().view(output.size()[0], output.size()[1], 1)
+            # (time, batch_size, 1) * (time, batch_size, hidden_state)
+            output = torch.sum(output * keys_view, 0)
+
+            return self.out(output), keys, sparsity_coherence_cost  # (batch_size, time)
+
+        return self.out(hidden)
+
+
+def get_multiclass_recall(preds, y_label):
+    # preds: (label_size), y_label; (label_size)
+    label_cat = range(len(label_list))
+    labels_accu = {}
+
+    for la in label_cat:
+        # for each label, we get the index of the correct labels
+        idx_of_cat = y_label == la
+        cat_preds = preds[idx_of_cat]
+        if cat_preds.size != 0:
+            accu = np.mean(cat_preds == la)
+            labels_accu[la] = [accu]
+        else:
+            labels_accu[la] = []
+
+    return labels_accu
+
+
+def get_multiclass_prec(preds, y_label):
+    label_cat = range(len(label_list))
+    labels_accu = {}
+
+    for la in label_cat:
+        # for each label, we get the index of predictions
+        idx_of_cat = preds == la
+        cat_preds = y_label[idx_of_cat]  # ground truth
+        if cat_preds.size != 0:
+            accu = np.mean(cat_preds == la)
+            labels_accu[la] = [accu]
+        else:
+            labels_accu[la] = []
+
+    return labels_accu
+
+
+def cumulate_multiclass_accuracy(total_accu, labels_accu):
+    for k, v in labels_accu.iteritems():
+        total_accu[k].extend(v)
+
+
+def get_mean_multiclass_accuracy(total_accu):
+    new_dict = {}
+    for k, v in total_accu.iteritems():
+        new_dict[k] = np.mean(total_accu[k])
+    return new_dict
+
+
+def eval_model(model, valid_iter, save_pred=False):
+    # when test_final is true, we save predictions
+    model.eval()
+    criterion = nn.CrossEntropyLoss()
+    correct = 0.0
+    cnt = 0
+    total_loss = 0.0
+    total_sparsity_coherence_cost = 0.0
+    total_labels_recall = None
+
+    total_labels_prec = None
+    all_preds = []
+    all_y_labels = []
+    all_orig_texts = []
+    all_keys = []
+    for data in valid_iter:
+        (x, x_lengths), y = data.Text, data.Description
+        if args.attn:
+            output, keys, sparsity_coherence_cost = model(x, x_lengths)
+            all_keys.extend(keys.data.cpu().numpy().tolist())
+        else:
+            output = model(x)
+        loss = criterion(output, y)
+        total_loss += loss.data[0] * x.size(1)
+
+        total_sparsity_coherence_cost += sparsity_coherence_cost.data[0] * x.size(1)
+
+        preds = output.data.max(1)[1]  # already taking max...I think, max returns a tuple
+        correct += preds.eq(y.data).cpu().sum()
+        cnt += y.numel()
+
+        orig_text = TEXT.reverse(x.data)
+        all_orig_texts.extend(orig_text)
+
+        if save_pred:
+            all_preds.extend(preds.cpu().numpy().tolist())
+            all_y_labels.extend(y.data.cpu().numpy().tolist())
+
+        # compute multiclass
+        labels_recall = get_multiclass_recall(preds.cpu().numpy(), y.data.cpu().numpy())
+        labels_prec = get_multiclass_prec(preds.cpu().numpy(), y.data.cpu().numpy())
+        if total_labels_recall is None:
+            total_labels_recall = labels_recall
+            total_labels_prec = labels_prec
+        else:
+            cumulate_multiclass_accuracy(total_labels_recall, labels_recall)
+            cumulate_multiclass_accuracy(total_labels_prec, labels_prec)
+
+            # valid and tests can stop themselves
+
+    multiclass_recall_msg = 'Multiclass Recall - '
+    mean_multi_recall = get_mean_multiclass_accuracy(total_labels_recall)
+
+    for k, v in mean_multi_recall.iteritems():
+        multiclass_recall_msg += labels[k] + ": " + str(v) + " "
+
+    multiclass_prec_msg = 'Multiclass Precision - '
+    mean_multi_prec = get_mean_multiclass_accuracy(total_labels_prec)
+
+    for k, v in mean_multi_prec.iteritems():
+        multiclass_prec_msg += labels[k] + ": " + str(v) + " "
+
+    logging.info(multiclass_recall_msg)
+
+    logging.info(multiclass_prec_msg)
+
+    if save_pred:
+        import csv
+        # we store things out, hopefully they are in correct order
+        with open(pjoin(args.run_dir, 'confusion_test.csv'), 'wb') as csvfile:
+            fieldnames = ['preds', 'labels', 'text']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for pair in zip(all_preds, all_y_labels, all_orig_texts):
+                writer.writerow({'preds': pair[0], 'labels': pair[1], 'text': pair[2]})
+        with open(pjoin(args.run_dir, 'attn_map.json'), 'wb') as f:
+            json.dump([all_preds, all_y_labels, all_orig_texts, all_keys], f)
+        with open(pjoin(args.run_dir, 'label_map.txt'), 'wb') as f:
+            json.dump(label_list, f)
+
+    model.train()
+    return correct / cnt, total_sparsity_coherence_cost / cnt
+
+
+def train_module(model, optimizer,
+                 train_iter, valid_iter, test_iter, max_epoch):
+    model.train()
+    criterion = nn.CrossEntropyLoss()
+
+    exp_cost = None
+    end_of_epoch = True  # False  # set true because we want immediate feedback...
+    iter = 0
+    best_valid = 0.
+    epoch = 1
+
+    for n in range(max_epoch):
+        for data in train_iter:
+            iter += 1
+
+            model.zero_grad()
+            (x, x_lengths), y = data.Text, data.Description
+
+            if args.attn:
+                output, keys, sparsity_coherence_cost = model(x, x_lengths)
+            else:
+                output = model(x)
+
+            loss = criterion(output, y)
+            if not args.sparsity_reg:
+                loss.backward()
+            else:
+                total_loss = loss + sparsity_coherence_cost
+                total_loss.backward()
+
+            torch.nn.utils.clip_grad_norm(model.parameters(), args.clip_grad)
+
+            optimizer.step()
+
+            if not exp_cost:
+                exp_cost = loss.data[0]
+            else:
+                exp_cost = 0.99 * exp_cost + 0.01 * loss.data[0]
+
+            if iter % 100 == 0:
+                logging.info("iter {} lr={} train_loss={} exp_cost={} sparsity_coherence_cost={} \n".format(iter,
+                                                                                                            optimizer.param_groups[
+                                                                                                                0][
+                                                                                                                'lr'],
+                                                                                                            loss.data[
+                                                                                                                0],
+                                                                                                            exp_cost,
+                                                                                                            sparsity_coherence_cost.data[
+                                                                                                                0]))
+
+        valid_accu, valid_sparsity_coherence_cost = eval_model(model, valid_iter)
+        sys.stdout.write("epoch {} lr={:.6f} train_loss={:.6f} valid_acc={:.6f} valid_sparsity_cost={:6f}\n".format(
+            epoch,
+            optimizer.param_groups[0]['lr'],
+            loss.data[0],
+            valid_accu,
+            valid_sparsity_coherence_cost
+        ))
+
+        epoch += 1
+
+        if valid_accu > best_valid:
+            best_valid = valid_accu
+
+        sys.stdout.write("\n")
+
+
+def tokenizer(text):  # create a tokenizer function
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+def init_emb(vocab, init="randn", num_special_toks=2):
+    # we can try randn or glorot
+    # mode="unk"|"all", all means initialize everything
+    emb_vectors = vocab.vectors
+    sweep_range = len(vocab)
+    running_norm = 0.
+    num_non_zero = 0
+    total_words = 0
+    for i in range(num_special_toks, sweep_range):
+        if len(emb_vectors[i, :].nonzero()) == 0:
+            # std = 0.5 is based on the norm of average GloVE word vectors
+            if init == "randn":
+                torch.nn.init.normal(emb_vectors[i], mean=0, std=0.5)
+        else:
+            num_non_zero += 1
+            running_norm += torch.norm(emb_vectors[i])
+        total_words += 1
+    logger.info("average GloVE norm is {}, number of known words are {}, total number of words are {}".format(
+        running_norm / num_non_zero, num_non_zero, total_words))
+
+
+if __name__ == '__main__':
+    import spacy
+
+    # spacy_en = spacy.load('en')
+    spacy_en = spacy.load('en_core_web_sm')
+
+    with open('../../data/clinvar/text_classification_db_labels.json', 'r') as f:
+        labels = json.load(f)
+
+    # map labels to list
+    label_list = [None] * len(labels)
+    for k, v in labels.items():
+        label_list[v] = k
+
+    labels = label_list
+    logger.info("available labels: ")
+    logger.info(labels)
+
+    TEXT = data.ReversibleField(sequential=True, tokenize=tokenizer,
+                                lower=True, include_lengths=True)
+    LABEL = data.Field(sequential=False, use_vocab=False)
+    if args.dataset == 'major':
+        train, val, test = data.TabularDataset.splits(
+            path='../../data/csu/', train='maj_label_train.tsv',
+            validation='maj_label_valid.tsv',
+            test='maj_label_test.tsv', format='tsv',
+            fields=[('Text', TEXT), ('Description', LABEL)])
+    else:
+        raise NotImplementedError
+        # train, val, test = data.TabularDataset.splits(
+        #     path='../../data/csu/', train='text_classification_db_train.tsv',
+        #     validation='text_classification_db_valid.tsv', test='text_classification_db_test.tsv', format='tsv',
+        #     fields=[('Text', TEXT), ('Description', LABEL)])
+
+    TEXT.build_vocab(train, vectors="glove.6B.100d")
+
+    # do repeat=False
+    train_iter, val_iter, test_iter = data.Iterator.splits(
+        (train, val, test), sort_key=lambda x: len(x.Text),  # no global sort, but within-batch-sort
+        batch_sizes=(32, 256, 256), device=args.gpu,
+        sort_within_batch=True, repeat=False)  # stop infinite runs
+    # if not labeling sort=False, then you are sorting through valid and test
+
+    vocab = TEXT.vocab
+
+    if args.rand_unk:
+        init_emb(vocab, init="randn")
+
+    # so now all you need to do is to create an iterator
+
+    model = Model(vocab, nclasses=len(labels), scaled_dot_attn=args.attn)
+    if torch.cuda.is_available():
+        model.cuda(args.gpu)
+
+    sys.stdout.write("num of parameters: {}\n".format(
+        sum(x.numel() for x in model.parameters() if x.requires_grad)
+    ))
+
+    need_grad = lambda x: x.requires_grad
+    optimizer = optim.Adam(
+        filter(need_grad, model.parameters()),
+        lr=0.001)
+    # optimizer = optim.SGD(
+    #     filter(need_grad, model.parameters()),
+    #     lr=0.01)
+
+    train_module(model, optimizer, train_iter, val_iter, test_iter,
+                 max_epoch=5)
+
+    test_accu, test_sparsity_coherence_cost = eval_model(model, test_iter, save_pred=True)
+    logger.info("final test accu: {}, test sparsity cost: {}".format(test_accu, test_sparsity_coherence_cost))
