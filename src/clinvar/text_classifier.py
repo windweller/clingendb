674d7c72eaa5a677a85ac61fa563e5f992de8756
windweller <leo.niecn@gmail.com>
2018-04-20 12:01:09 -0700

    fix an error

--- src/clinvar/text_classifier.py
+++ src/clinvar/text_classifier.py
@@ -187,7 +187,7 @@ class Model(nn.Module):
 
             return self.out(output), keys, sparsity_coherence_cost  # (batch_size, time)
 
-        return self.out(hidden)
+        return self.out(hidden[0])
 
 
 def get_multiclass_recall(preds, y_label):
