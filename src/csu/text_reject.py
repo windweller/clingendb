f80ae848f4c524f6c133fbe4c81b2f4d7d9f7c58
windweller <leo.niecn@gmail.com>
2018-05-17 18:19:21 -0700

    fix small error

--- src/csu/text_reject.py
+++ src/csu/text_reject.py
@@ -780,7 +780,7 @@ def train_module(model, optimizer,
                     exp_cost = 0.99 * exp_cost + 0.01 * loss.data[0]
 
                 if iter % 100 == 0:
-                    avg_rej_rate = sum(training_rejectiong_rate) / float(len(training_rejectiong_rate))
+                    avg_rej_rate = sum(training_rejectiong_rate) / float(max(len(training_rejectiong_rate), 1.))
 
                     logging.info(
                         "iter {} lr={} train_loss={} exp_cost={} rej={} \n".format(iter,
