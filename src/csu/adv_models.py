15159661acd790e85804ba84e8b80e33c9674986
windweller <leo.niecn@gmail.com>
2018-06-10 23:36:37 -0700

    removed decoder

--- src/csu/adv_models.py
+++ src/csu/adv_models.py
@@ -209,12 +209,10 @@ class EncoderDecoder(nn.Module):
         return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
 
 class Classifier(nn.Module):
-    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+    def __init__(self, encoder, src_embed, generator):
         super(Classifier, self).__init__()
         self.encoder = encoder
-        self.decoder = decoder
         self.src_embed = src_embed
-        self.tgt_embed = tgt_embed
         self.generator = generator
 
         # instead of using tgt_emb or decoder...we simply...
@@ -229,9 +227,6 @@ class Classifier(nn.Module):
 
     def encode(self, src, src_mask):
         return self.encoder(self.src_embed(src), src_mask)
-
-    def decode(self, memory, src_mask, tgt, tgt_mask):
-        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
 
 class Generator(nn.Module):
     "Define standard linear + softmax generation step."
@@ -336,10 +331,10 @@ def make_model(src_vocab, config, label_size=42, N=6,
     # EncoderDecoder
     model = Classifier(
         Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
-        Decoder(DecoderLayer(d_model, c(attn), c(attn),
-                             c(ff), dropout), N),
+        # Decoder(DecoderLayer(d_model, c(attn), c(attn),
+        #                      c(ff), dropout), N),
         nn.Sequential(Embeddings(d_model, src_vocab, config), c(position)),
-        nn.Sequential(Embeddings(d_model, [0.], config), c(position)),
+        # nn.Sequential(Embeddings(d_model, [0.], config), c(position)),
         Generator(d_model, label_size))
 
     # This was important from their code.
